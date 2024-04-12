78c34d4ab5a050f5fba49ebc522d5faa1f5fc93a
not-good-keeper <nethi.kushaal@gmail.com>
2024-04-12 22:40:07 +0530

    modified:   .gitignore 	new file:   js/firebaseConfig.json.enc 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,28 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 
+const encryptedApiKey = require('../apiKey.json.enc');
+const encryptedAuthDomain = require('../authDomain.json.enc');
+const encryptedDatabaseURL = require('../databaseURL.json.enc');
+const encryptedProjectId = require('../projectId.json.enc');
+const encryptedStorageBucket = require('../storageBucket.json.enc');
+const encryptedMessagingSenderId = require('../messagingSenderId.json.enc');
+const encryptedAppId = require('../appId.json.enc');
+
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  apiKey: decrypt(encryptedApiKey),
+  authDomain: decrypt(encryptedAuthDomain),
+  databaseURL: decrypt(encryptedDatabaseURL),
+  projectId: decrypt(encryptedProjectId),
+  storageBucket: decrypt(encryptedStorageBucket),
+  messagingSenderId: decrypt(encryptedMessagingSenderId),
+  appId: decrypt(encryptedAppId)
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// ... rest of your code
+
 
 // Now you can use app, auth, db, and storage as you have them
 const auth = getAuth();
